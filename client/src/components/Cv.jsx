181dc7e2d6a8d871e21084877112b07adaa0240a
freestyle09 <jozefrzadkosz09@gmail.com>
2019-02-26 18:37:45 +0100

    added cv languages

--- client/src/components/Cv.jsx
+++ client/src/components/Cv.jsx
@@ -1,8 +1,16 @@
 import React, { Component } from 'react';
 import Menu from './Menu';
+import Footer from './Footer';
+
 class Cv extends Component {
   state = {
     language: 'en'
+  };
+
+  changeLanguage = string => {
+    this.setState({
+      language: string
+    });
   };
   render() {
     return (
@@ -13,7 +21,34 @@ class Cv extends Component {
             <h1>cv</h1>
             <Menu />
           </div>
+          <div className='languages'>
+            <div>
+              <a className='language' onClick={() => this.changeLanguage('en')}>
+                <img src='/united-kingdom.png' alt='' />
+              </a>
+              <a className='language' onClick={() => this.changeLanguage('pl')}>
+                <img src='/poland.png' alt='' />
+              </a>
+            </div>
+            {this.state.language === 'pl' && (
+              <a className='download' href='/cv_pl.pdf' download='JózefRzadkosz_cv_pl'>
+                <i className='fas fa-file-download' />
+              </a>
+            )}
+            {this.state.language === 'en' && (
+              <a className='download' href='/header_train.jpg' download='JózefRzadkosz_cv_en'>
+                <i className='fas fa-file-download' />
+              </a>
+            )}
+          </div>
+          {this.state.language === 'pl' && (
+            <div className='cv'>
+              <img src='/cv_pl.png' alt='' />
+            </div>
+          )}
+          {this.state.language === 'en' && <div>English</div>}
         </div>
+        <Footer />
       </section>
     );
   }
